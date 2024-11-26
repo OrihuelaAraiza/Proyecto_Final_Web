480cc3347850e860e8f8ac2f12608a431fe8d3eb
Feintt <0253214@up.edu.mx>
2024-11-25 18:22:31 -0600

    Latest stable

--- src/components/orders/ProjectsGallery.jsx
+++ src/components/orders/ProjectsGallery.jsx
@@ -5,22 +5,22 @@ const projectData = [{
     id: 1,
     name: "Clean Water Initiative",
     description: "Providing clean and safe water to communities.",
-    image: "https://images.unsplash.com/photo-1479660656269-197ebb83b540?dpr=2&auto=compress,format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb&crop=",
+    image: "/projects/img.png",
 }, {
     id: 2,
     name: "Education for All",
     description: "Empowering children with quality education.",
-    image: "https://images.unsplash.com/photo-1479659929431-4342107adfc1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=",
+    image: "/projects/img_1.png",
 }, {
     id: 3,
     name: "Health Outreach Program",
     description: "Providing medical services to underserved communities.",
-    image: "https://images.unsplash.com/photo-1479644025832-60dabb8be2a1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=",
+    image: "/projects/img_2.png",
 }, {
     id: 4,
     name: "Environmental Awareness",
     description: "Raising awareness for environmental conservation.",
-    image: "https://images.unsplash.com/photo-1479621051492-5a6f9bd9e51a?dpr=2&auto=compress,format&fit=crop&w=1199&h=811&q=80&cs=tinysrgb&crop=",
+    image: "/projects/img_3.png",
 },];
 
 const ProjectGallery = () => {
